81a9653f0b98c7d8930957cae4bd3e889791ea79
SpykeX3 <spykex3@gmail.com>
2020-08-03 17:56:02 +0700

    react main page

--- js/app.js
+++ js/app.js
@@ -1,13 +1,6 @@
-new Vue({
-    el: '#app',
-    data () {
-        return {
-            info: null
-        }
-    },
-    mounted () {
-        axios
-            .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-            .then(response => (this.info = response))
-    }
-})
+import React from 'react';
+import ReactDOM from 'react-dom';
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
+);
